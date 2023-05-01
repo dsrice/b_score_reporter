017f4a68cbcbba9d3bb806f5c2a25d088e3bbf8c
dsrice <v.fuji.0223@gmail.com>
2023-05-01 17:35:05 +0900

    generateコマンドの処理を整理して、File作成に変更

--- login/infra/generator/gonerate.go
+++ login/infra/generator/gonerate.go
@@ -3,18 +3,25 @@ package generator
 import (
 	"fmt"
 	"github.com/dave/jennifer/jen"
+	"github.com/labstack/gommon/log"
 )
 
 type CodeInfo struct {
-	PkIFName    string // インターフェイス側パッケージ名称
-	PkCTName    string // コンテナ側パッケージ名称
-	PkMockName  string // モック側パッケージ名称
-	IFPath      string
-	CTPath      string
-	MockPath    string
-	PublicName  string
-	PrivateName string
-	MockFlg     bool
+	PublicName   string
+	PrivateName  string
+	PkIFName     string // インターフェイス側パッケージ名称
+	PkCTName     string // コンテナ側パッケージ名称
+	PkMockName   string // モック側パッケージ名称
+	IFPath       string
+	CTPath       string
+	MockPath     string
+	IFName       string
+	CTName       string
+	MockName     string
+	IFFileName   string
+	CTFileName   string
+	MockFileName string
+	MockFlg      bool
 }
 
 func (info *CodeInfo) SetPackageInfo(command string) {
@@ -22,24 +29,40 @@ func (info *CodeInfo) SetPackageInfo(command string) {
 	case "controller":
 		info.PkIFName = "handlers"
 		info.PkCTName = "controllers"
-		info.IFPath = "app/controllers/handlers"
-		info.CTPath = "app/controllers"
+		info.IFPath = "login/controllers/handlers"
+		info.CTPath = "login/controllers"
+		info.IFName = info.PublicName + "Handler"
+		info.CTName = info.PrivateName + "Controller"
+		info.IFFileName = info.PublicName + "Handler.go"
+		info.CTFileName = info.PublicName + "Controller.go"
 		info.MockFlg = false
 	case "usecase":
 		info.PkIFName = "interfaces"
 		info.PkCTName = "usecases"
-		info.IFPath = "app/usecases/interfaces"
-		info.CTPath = "app/usecases"
+		info.IFPath = "login/usecases/interfaces"
+		info.CTPath = "login/usecases"
 		info.PkMockName = "mockusecases"
-		info.MockPath = "app/usecases/mockusecases"
+		info.MockPath = "login/usecases/mockusecases"
+		info.IFName = info.PublicName + "Interface"
+		info.CTName = info.PrivateName + "UseCase"
+		info.MockName = "Mock" + info.PublicName + "UseCase"
+		info.IFFileName = info.PublicName + "Interface.go"
+		info.CTFileName = info.PublicName + "UseCase.go"
+		info.MockFileName = "Mock" + info.PublicName + "UseCase.go"
 		info.MockFlg = true
 	case "repository":
 		info.PkIFName = "adapters"
 		info.PkCTName = "repositories"
-		info.IFPath = "app/repositories/adapters"
-		info.CTPath = "app/repositories"
+		info.IFPath = "login/repositories/adapters"
+		info.CTPath = "login/repositories"
 		info.PkMockName = "mockrepositories"
-		info.MockPath = "app/repositories/mockrepositories"
+		info.MockPath = "login/repositories/mockrepositories"
+		info.IFName = info.PublicName + "Adapter"
+		info.CTName = info.PrivateName + "Repository"
+		info.MockName = "Mock" + info.PublicName + "Repository"
+		info.IFFileName = info.PublicName + "Adapter.go"
+		info.CTFileName = info.PublicName + "Repository.go"
+		info.MockFileName = "Mock" + info.PublicName + "Repository.go"
 		info.MockFlg = true
 	}
 }
@@ -56,31 +79,45 @@ func (info CodeInfo) CreateCode() {
 func createInterface(info CodeInfo) {
 	f := jen.NewFile(info.PkIFName)
 
-	f.Type().Id(info.PublicName).Interface()
+	f.Type().Id(info.IFName).Interface()
 
-	fmt.Printf("%#v", f)
+	err := f.Save("../" + info.IFPath + "/" + info.IFFileName)
+	if err != nil {
+		log.Error(err.Error())
+	}
 }
 
 func createContainer(info CodeInfo) {
 	f := jen.NewFile(info.PkCTName)
 
-	f.Type().Id(info.PrivateName).Struct()
+	f.ImportName(info.IFPath, info.PkIFName)
+
+	f.Type().Id(info.CTName).Struct()
 
 	f.Func().
 		Id(fmt.Sprintf("New"+info.PublicName)).
 		Params().
-		Qual(info.IFPath, info.PublicName).
-		Block()
+		Qual(info.IFPath, info.IFName).
+		Block(
+			jen.Return(jen.Op("&").Id(info.CTName).Block()))
 
-	fmt.Printf("%#v", f)
+	err := f.Save("../" + info.CTPath + "/" + info.CTFileName)
+	if err != nil {
+		log.Error(err.Error())
+	}
 }
 
 func createMock(info CodeInfo) {
 	f := jen.NewFile(info.PkMockName)
 
-	f.Type().Id("Mock" + info.PublicName).Struct(
+	f.ImportName("github.com/stretchr/testify/mock", "mock")
+
+	f.Type().Id(info.MockName).Struct(
 		jen.Qual("github.com/stretchr/testify/mock", "Mock"),
 	)
 
-	fmt.Printf("%#v", f)
+	err := f.Save("../" + info.MockPath + "/" + info.MockFileName)
+	if err != nil {
+		log.Error(err.Error())
+	}
 }
